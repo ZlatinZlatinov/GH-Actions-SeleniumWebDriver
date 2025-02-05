6da90c675b14b3ea7a1d1be36d22d8c806853ae8
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-02-05 21:51:32 +0200

    Update driver URL

--- TestProject1/TestCalculator.cs
+++ TestProject1/TestCalculator.cs
@@ -33,7 +33,7 @@ namespace TestProject1
 
             driver = new ChromeDriver(options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Url = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com/number-calculator/";
+            driver.Url = "http://127.0.0.1:5500/";
 
             textBoxFirstNum = driver.FindElement(By.Id("number1"));
             dropDownOperation = driver.FindElement(By.Id("operation"));
